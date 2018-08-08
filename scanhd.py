8a558bcd619d095b7269cae90c3efc14d8bbd2e2
ekravitz <42222707+ekravitz@users.noreply.github.com>
2018-08-08 19:52:36 -0400

    Update scanhd.py

--- scanhd.py
+++ scanhd.py
@@ -1,14 +1,24 @@
+import os
 import pickle
 import md5
 
-try:
-    l = pickle.load(open("db"))
-except IOError:
-    l = []
-db = dict(l)
-path = "/etc/hosts"
-checksum = md5.md5(open(path).read())
-if db.get(path, None) != checksum:
-    print "file changed"
-    db[path] = checksum
-pickle.dump(db.items(), open("db", "w")
+#Python 3
+
+def scan_for_changes(topdir=".")
+	try:
+		l = pickle.load(open(topdir+"\db"))
+	except IOError:
+		l = []
+	db = dict(l)
+
+	for dirpath, dirnames, files in os.walk(topdir):
+		for name in files:
+			if name.lower().endswith(exten):
+				print(os.path.join(dirpath, name))
+	
+	checksum = md5.md5(open(path).read())
+	
+	if db.get(path, None) != checksum:
+		print "file changed"
+		db[path] = checksum
+	pickle.dump(db.items(), open("db", "w")
