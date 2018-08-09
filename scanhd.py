fa7a28589fa0655c46e0dbc1b59e133612cff9ba
ekravitz <42222707+ekravitz@users.noreply.github.com>
2018-08-09 00:29:00 -0400

    Update scanhd.py

--- scanhd.py
+++ scanhd.py
@@ -11,10 +11,15 @@ class MyFile():
 #Auto-init and save file attributes that are relevant
 	def __init__(self, path):
 		self.path = path
-		self.st_size = os.stat(path).st_size
-		self.st_mtime= os.stat(path).st_mtime
+		
+		#self.st_size = os.stat(path).st_size
+		#self.st_mtime= os.stat(path).st_mtime
 		self.checksum = hashlib.md5(open(path,mode='rb').read())
 
+		self.st_size=1
+		self.st_mtime=1
+		self.checksum=1
+		
 	def checkSame():
 		#check file size and modification date
 		if (self.st_size != os.stat(self.path).st_size) or (self.st_mtime != os.stat(self.path).st_mtime):
